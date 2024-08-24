70dfbb5f031f218238fee5b15a7c3cf7d1256097
Vin-it-git <palvinit33@gmail.com>
2024-08-24 11:20:02 +0530

    feature-done added

--- todoapp/src/component/TodoItem.jsx
+++ todoapp/src/component/TodoItem.jsx
@@ -3,14 +3,18 @@ export default function TodoItems({ item, todos, setTodos }) {
   function handleDelete(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
-  function handlename(name){
-    console.log('item cliked',name)
+  function handlename(name) {
+    const newArray = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray);
   }
+  const newclassName = item.done ? styles.completed : ""
   return (
     <div className={styles.item}>
       <div className={styles.itemname}>
-        <span onClick={()=>handlename(item.name)}>{item.name}</span>
-        
+        <span className={newclassName} onClick={() => handlename(item.name)}>{item.name}</span>
+
         <span>
           <button
             onClick={() => handleDelete(item)}
